826633401530507e9558e18726c2dab1777a7ada
arturo-22 <arturo_romero_15@hotmail.com>
2025-03-28 12:22:40 -0500

    probando-subida

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Base para la rama `master`
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
+  base: '/users-management-app/', // ¡VERIFICA QUE ESTE EXACTO!
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 });
